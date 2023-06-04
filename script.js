b1007b482795fbe653b38d82e1dda7ffded11363
Csampy-Nyul <132491677+Csampy-Nyul@users.noreply.github.com>
2023-06-04 11:18:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+menu = document.getElementById("menu");
+
+const openMenu = () => {
+    menu.style.transform = "translate(0%)";
+}
+
+const closeMenu = () => {
+    menu.style.transform = "translate(100%)";
+}
