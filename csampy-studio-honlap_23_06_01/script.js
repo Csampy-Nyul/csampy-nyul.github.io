3b681256848458f5522204c2d29095974182edbf
Csampy-Nyul <132491677+Csampy-Nyul@users.noreply.github.com>
2023-06-04 11:08:09 +0200

    Delete csampy-studio-honlap_23_06_01 directory

--- csampy-studio-honlap_23_06_01/script.js
+++
@@ -1,9 +0,0 @@
-menu = document.getElementById("menu");
-
-const openMenu = () => {
-    menu.style.transform = "translate(0%)";
-}
-
-const closeMenu = () => {
-    menu.style.transform = "translate(100%)";
-}
